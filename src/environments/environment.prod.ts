87926847182af8349d7548532726a9a5bb78682e
Kamil <kamilkasprzak1999@gmail.com>
2021-08-28 15:51:09 +0200

    get token from spring boot and loading spinner

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  spotifyApp: {
+    client_id: 'c5de057c69af43f78132e18432ab8060',
+    redirect_uri: 'http://localhost:4200/login/',
+    scope: 'user-top-read'
+    }
+};
